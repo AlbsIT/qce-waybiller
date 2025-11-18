a5f5331018f1485a471efaecd9897b0a9034f6f1
syfer <seiferalbacete@gmail.com>
2025-11-18 09:32:00 +0000

    fix: adjustments again

--- src/components/print.tsx
+++ src/components/print.tsx
@@ -33,7 +33,7 @@ const PrintDocData = ({ p }: { p: PrintFormSchema }) => {
   return (
     <Page size={{ width: "6.5in", height: "4in" }}>
       <View>
-        <Text style={{ ...styles.text, top: "0.8in", left: "0.4in" }}>{date.toLocaleDateString()}</Text>
+        <Text style={{ ...styles.text, top: "0.7in", left: "0.45in" }}>{date.toLocaleDateString()}</Text>
         <Text style={{ ...styles.text, top: "0.9in", left: "0.9in" }}>{p.shipperName}</Text>
         <Text style={{ ...styles.text, top: "1.2in", left: "2.1in" }}>{p.shipperContactNo}</Text>
         <Text style={{ ...styles.text, top: "1.2in", left: "0.4in" }}>{p.shipperAddress}</Text>
@@ -42,7 +42,7 @@ const PrintDocData = ({ p }: { p: PrintFormSchema }) => {
         <Text style={{ ...styles.text, top: "1.2in", left: "5.5in" }}>{p.consigneeContactNo}</Text>
         <Text style={{ ...styles.text, top: "1.2in", left: "3.3in" }}>{p.consigneeAddress}</Text>
         <Text style={{ ...styles.text, top: "1.9in", left: "3.3in" }}>{p.deliveryInstructions}</Text>
-        <Text style={{ ...styles.text, top: "3.1in", left: "0.5in" }}>{p.declaredValue}</Text>
+        <Text style={{ ...styles.text, top: "3.2in", left: "1n" }}>{p.declaredValue}</Text>
       </View>
     </Page>
   );
